67e6646947e930f935c21318439bd424cd9fcd4d
Rodolfo Mori <rodolfomori@Rodolfos-MacBook-Pro.local>
2024-05-01 17:21:46 -0300

    projeto finalizado

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function generateNumber() {
+  const min = Math.ceil(document.querySelector('.input-min').value)
+  const max = Math.floor(document.querySelector('.input-max').value)
+
+  if (max > min) {
+    const result = Math.floor(Math.random() * (max - min + 1)) + min
+
+    alert(result)
+  } else {
+    alert('O valor minimo tem que ser MENOR que o valor máximo')
+  }
+}
